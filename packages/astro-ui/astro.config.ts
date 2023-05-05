6565d0f14d648c5ba064d11486a4c04fffb9708e
Pavel Mayorov <TA-19V-169@tptlive.ee>
2023-05-05 11:10:17 +0300

    feat: added type aliases, fixed eslint config, added 'ts-reset'

--- packages/astro-ui/astro.config.ts
+++ packages/astro-ui/astro.config.ts
@@ -1,4 +1,20 @@
 import { defineConfig } from 'astro/config'
 
+import node from '@astrojs/node'
+import unocss from 'unocss/astro'
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  // https://docs.astro.build/en/guides/server-side-rendering/
+  output: 'server',
+
+  // https://docs.astro.build/en/guides/integrations-guide/node/
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  integrations: [
+    // https://unocss.dev/integrations/astro
+    unocss(),
+  ],
+})
