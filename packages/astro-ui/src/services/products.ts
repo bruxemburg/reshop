4508054a98f27d280e6b6ea621c94892da2cb9a7
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2023-05-08 12:24:37 +0300

    feat: added 'useGraphQL' composable, added fetch from api to render product details page, fixed hasura public role env variable

---
+++ packages/astro-ui/src/services/products.ts
@@ -0,0 +1,69 @@
+import { useGraphQL } from '~/composables/graphql'
+
+export interface Entity {
+  id: number
+  date_created: string
+  date_updated: string
+}
+
+export interface Product extends Entity {
+  name: string
+  price: number
+  description: string
+  category: Category
+
+  /** Used to calculate total reviews for product */
+  reviews_aggregate: { aggregate: { count: number } }
+
+  /** products images */
+  products_files: Array<{ directus_file: Image }>
+  thumbnail: string
+}
+
+export interface Category extends Entity {
+  name: string
+}
+
+interface Image {
+  id: string
+  filename_download: string
+  description: string
+}
+
+export async function fetchProductById(id: string) {
+  const { products_by_pk: product } = await useGraphQL<{ products_by_pk: Product | null }>(`
+    query getProductById($id: Int!) {
+      products_by_pk(id: $id) {
+        id,
+        name,
+        price,
+        description,
+        date_created,
+        date_updated,
+        category {
+          id,
+          name,
+          date_created,
+          date_updated
+        },
+        reviews_aggregate {
+          aggregate {
+            count
+          }
+        },
+        products_files {
+          directus_file {
+            id,
+            description,
+            filename_download
+          }
+        },
+        thumbnail
+      }
+    }
+  `, {
+    id,
+  })
+
+  return product
+}
